4eb19db893b388ba4590ff19cd3f326fa0e876c5
muchsite <muchsite3@gmail.com>
2023-12-04 03:16:51 +0400

    course

--- src/pages/courses/CourseOne.jsx
+++ src/pages/courses/CourseOne.jsx
@@ -5,6 +5,8 @@ import "./one.scss";
 import { useParams } from "react-router-dom";
 import LoadnigMain from "../../components/loading/LoadnigMain";
 import Loading from "../../components/loading/Loading";
+import { FaWindowClose } from "react-icons/fa";
+import logo from "../../images/BlackLogo.png";
 const CourseOne = () => {
   const { baseURL } = useMainContext();
   const [data, setData] = useState({});
@@ -18,7 +20,9 @@ const CourseOne = () => {
   const [tId, setTId] = useState("");
   const [age, setAge] = useState("");
   const [sending, setSending] = useState(false);
+  const [open, setOpen] = useState(false);
   const [sent, setSent] = useState(false);
+  const [error, setError] = useState({});
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
@@ -37,6 +41,7 @@ const CourseOne = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     setSending(true);
+    setError({});
     try {
       const res = await axios.post(
         baseURL + `api/course/${data.course.slug}/`,
@@ -63,97 +68,118 @@ const CourseOne = () => {
       alert("Message has been sent");
     } catch (error) {
       setSending(false);
+      setError(error.response.data);
       console.log(error);
     }
   };
+  console.log(error);
   return (
     <>
       {loading ? (
         <LoadnigMain />
       ) : (
         <div className="s_course_container">
-          <h2 className="s_course_title"> {data.course?.c_title}</h2>
-          <img src={data.course?.c_img} alt="" className="s_course_img" />
-          <div className="s_course_info">
-            <p>{data.course?.cost}</p>
-            <p>{data.course?.facilitator}</p>
-          </div>
-          <div className="s_course_desc">
-            <div
-              dangerouslySetInnerHTML={{ __html: data.course?.c_desc }}
-            ></div>
-          </div>
-          <div className="s_course_form">
-            <form onSubmit={handleSubmit}>
-              <div className="s_courses_input_container">
-                <div className="s_course_input">
-                  <input
-                    type="text"
-                    placeholder="Name:"
-                    required
-                    value={name}
-                    onChange={(e) => setName(e.currentTarget.value)}
-                  />
-                </div>
-                <div className="s_course_input">
-                  <input
-                    type="text"
-                    placeholder="Email:"
-                    required
-                    value={email}
-                    onChange={(e) => setEmail(e.currentTarget.value)}
-                  />
-                </div>
-                <div className="s_course_input">
-                  <input
-                    type="number"
-                    placeholder="Nunber:"
-                    required
-                    value={number}
-                    onChange={(e) => setNumber(e.currentTarget.value)}
-                  />
-                </div>
-                <div className="s_course_input">
-                  <input
-                    type="text"
-                    placeholder="City:"
-                    required
-                    value={city}
-                    onChange={(e) => setCity(e.currentTarget.value)}
-                  />
-                </div>
-                <div className="s_course_input">
-                  <input
-                    type="text"
-                    placeholder="Job:"
-                    required
-                    value={job}
-                    onChange={(e) => setJob(e.currentTarget.value)}
-                  />
-                </div>
-                <div className="s_course_input">
-                  <input
-                    type="number"
-                    placeholder="Age:"
-                    required
-                    value={age}
-                    onChange={(e) => setAge(e.currentTarget.value)}
-                  />
-                </div>
-                <div className="s_course_input">
-                  <input
-                    type="number"
-                    placeholder="Transaction id:"
-                    required
-                    value={tId}
-                    onChange={(e) => setTId(e.currentTarget.value)}
-                  />
-                </div>
+          <div className={`s_course_form ${open && "top_0"}`}>
+            <div className="form_container">
+              <div className="form_images">
+                <img src={logo} alt="" />
+                <FaWindowClose onClick={() => setOpen(!open)} />
               </div>
-              <button type="submit">Send</button>
-              {sending && <Loading />}
-            </form>
+              <form onSubmit={handleSubmit}>
+                <div className="s_courses_input_container">
+                  <div className="s_course_input">
+                    <input
+                      type="text"
+                      placeholder="Name:"
+                      required
+                      value={name}
+                      onChange={(e) => setName(e.currentTarget.value)}
+                    />
+                  </div>
+                  <div
+                    className={`s_course_input ${
+                      "email" in error && "outline_red"
+                    }`}
+                  >
+                    <input
+                      type="text"
+                      placeholder="Email:"
+                      required
+                      value={email}
+                      onChange={(e) => setEmail(e.currentTarget.value)}
+                    />
+                    <p>{"email" in error && error.email}</p>
+                  </div>
+                  <div className="s_course_input">
+                    <input
+                      type="number"
+                      placeholder="Nunber:"
+                      required
+                      value={number}
+                      onChange={(e) => setNumber(e.currentTarget.value)}
+                    />
+                  </div>
+                  <div className="s_course_input">
+                    <input
+                      type="text"
+                      placeholder="City:"
+                      required
+                      value={city}
+                      onChange={(e) => setCity(e.currentTarget.value)}
+                    />
+                  </div>
+                  <div className="s_course_input">
+                    <input
+                      type="text"
+                      placeholder="Job:"
+                      required
+                      value={job}
+                      onChange={(e) => setJob(e.currentTarget.value)}
+                    />
+                  </div>
+                  <div className="s_course_input">
+                    <input
+                      type="number"
+                      placeholder="Age:"
+                      required
+                      value={age}
+                      onChange={(e) => setAge(e.currentTarget.value)}
+                    />
+                  </div>
+                  <div className="s_course_input">
+                    <input
+                      type="number"
+                      placeholder="Transaction id:"
+                      required
+                      value={tId}
+                      onChange={(e) => setTId(e.currentTarget.value)}
+                    />
+                  </div>
+                </div>
+                <div className="form_btn_container">
+                  <button type="submit">Send</button>
+                  {sending && <Loading color={"#c47843"} />}
+                </div>
+              </form>
+            </div>
           </div>
+          <div className="s_course_landing">
+            <img src={data.course?.c_img} alt="" className="s_course_img" />
+            <div className="s_course_info">
+              <h2 className="s_course_title"> {data.course?.c_title}</h2>
+              <p>Facilitator: {data.course?.facilitator}</p>
+              <p>
+                Price :{" "}
+                {data.course?.cost > 0 ? `${data.course?.cost}$` : "FREE!"}
+              </p>
+              <button onClick={() => setOpen(!open)}>Register Now!</button>
+            </div>
+          </div>
+
+          <div
+            dangerouslySetInnerHTML={{ __html: data.course?.c_desc }}
+            className="s_course_desc"
+          ></div>
         </div>
       )}
     </>
